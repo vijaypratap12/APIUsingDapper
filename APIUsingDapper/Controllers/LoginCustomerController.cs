fc5c722679d019e0d0063eeeae179e087689ee05
Madhu Patel <patelmadhu7470@gmail.com>
2023-04-14 11:42:25 +0530

    created Token

---
+++ APIUsingDapper/Controllers/LoginCustomerController.cs
@@ -0,0 +1,44 @@
+﻿using APIUsingDapper.DAL.Interfaces;
+using APIUsingDapper.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIUsingDapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginCustomerController : ControllerBase
+    {
+
+        private readonly IRestaurant _restaurant;
+        public LoginCustomerController(IRestaurant rest)
+        {
+            _restaurant = rest;
+        }
+
+        ///// <summary>
+        ///// loginCustomer
+        ///// </summary>
+        ///// <param name="loginCustomer"></param>
+        ///// <returns></returns>
+        //[HttpPost]
+        //[Route("LoginCustomer")]
+        //[AllowAnonymous]
+        //public async Task<IActionResult> LoginCustomer(LoginCustomer loginCustomer)
+        //{
+        //    TokenModel tokenModel = new TokenModel();
+        //    //string result;
+        //    try
+        //    {
+        //        tokenModel = await _restaurant.LoginCustomer(loginCustomer);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        //result = 0;
+        //        return BadRequest(ex.Message);
+        //    }
+        //    return Ok(tokenModel);
+        //}
+    }
+}
--- APIUsingDapper/Models/RestaurantModel.cs
+++ APIUsingDapper/Models/RestaurantModel.cs
@@ -1,38 +1,38 @@
-﻿using MongoDB.Bson.Serialization.IdGenerators;
-using System.Globalization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APIUsingDapper.Models
 {
-   
     public class CustomerDetails
     {
         public int? CustomerId { get; set; }
         public string? CustomerName { get; set; }
     }
-   
+
     public class AddCustomer
     {
         public string? CustomerName { get; set; }
         public string? CustomerContact { get; set; }
         public string? CustomerAddress { get; set; }
-        
+
     }
-   
+
     public class DeleteCustomer
     {
         public int CustomerId { get; set; }
     }
-    public class DeleteStaff 
-    { 
+
+    public class DeleteStaff
+    {
         public int StaffId { get; set; }
     }
     public class AllCustomerDetails
     {
         public int CustomerId { get; set; }
-        public string CustomerName { get; set; }    
+        public string CustomerName { get; set; }
         public string CustomerContact { get; set; }
         public string CustomerAddress { get; set; }
-        
+
     }
 
     public class FoodList
@@ -70,8 +70,27 @@ namespace APIUsingDapper.Models
     public class AddFeedback
     {
         public int CustomerId { get; set; }
-        public string FoodName { get; set; }
-        public string Messages { get; set; }
+        public string? FoodName { get; set; }
+        public string? Messages { get; set; }
+    }
+
+    public class UpdateFeedback
+    {
+        public string CustomerAddress { get; set; }
+    }
+
+    public class LoginCustomer
+    {
+        public int CustomerId { get; set; }
+        public string? CustomerName { get; set; }
+    }
+
+    public class TokenModel
+    {
+        public string token { get; set; }
+        public DateTime expires { get; set; }
+        public int CustomerId { get; set; }
+        public string? CustomerName { get; set; }      
     }
 }
 
