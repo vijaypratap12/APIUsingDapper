c6006e0b5c8d2c26780296ef11e76e407f1340f8
Madhu Patel <patelmadhu7470@gmail.com>
2023-04-17 16:45:20 +0530

    deleted irrelevant files

--- APIUsingDapper/Program.cs
+++ APIUsingDapper/Program.cs
@@ -8,63 +8,6 @@ using MongoDB.Libmongocrypt;
 using System.Reflection;
 using System.Text;
 
-<<<<<<< HEAD
-internal class Program
-=======
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddScoped<IUserProfile, UserProfileRepo>();
-builder.Services.AddScoped<IRestaurant, RestaurantRepo>();
-builder.Services.AddSwaggerGen(
-    options =>
-builder.Services.AddScoped<IUserProfile, UserProfileRepo>();
-//builder.Services.AddSingleton<IUserProfile, UserProfileRepo>();
-//builder.Services.AddTransient<IUserProfile, UserProfileRepo>(); 
-
-
-builder.Services.AddScoped<IEmployee,  EmployeeRepo>();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc(name: "v1", new OpenApiInfo { Title = "Api", Version = "v1" });
-    options.AddSecurityDefinition(name: "oauth2", new OpenApiSecurityScheme
-    {
-        options.SwaggerDoc(name: "v1", new OpenApiInfo { Title = "Api", Version = "v1" });
-        options.AddSecurityDefinition(name: "oauth2", new OpenApiSecurityScheme
-        {
-            Type = SecuritySchemeType.OAuth2,
-            Flows = new OpenApiOAuthFlows
-            {
-                ClientCredentials = new OpenApiOAuthFlow
-                {
-                    TokenUrl = new Uri("https://localhost:7177/connect/tokens"),
-                    Scopes = new Dictionary<string, string>
-            {
-                { "api", "API" }
-            }
-                }
-            }
-        });
-        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
-
-    });
-    options.OperationFilter<AuthorizeOperationFilter>();
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
-});
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
->>>>>>> 32919ed7af6a6636877052f8f224f250f7c73d5a
-{
-    private static void Main(string[] args)
-    {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllers();
 
@@ -143,5 +86,4 @@ if (app.Environment.IsDevelopment())
         app.MapControllers();
 
         app.Run();
-    }
-}
+
